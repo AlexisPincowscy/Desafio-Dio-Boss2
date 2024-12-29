2f50ea29fcf7b4d69e36b5f5cdbb54be4dcf3beb
AlexisPincowscy <alexispincowscy@hotmail.com>
2024-12-29 17:11:03 -0300

    versão final partida rankeada

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let saldoFinal = saldoVitorias(187, 115);
+let nivelHeroi = nivel(saldoFinal);
+console.log(`Seu saldo de vitórias é ${saldoFinal} e seu nível é ${nivelHeroi}`);
+
+
+function saldoVitorias (vitorias = 0, derrotas = 0) {
+    return vitorias - derrotas;
+}
+
+function nivel (saldoVitorias){
+    if (saldoVitorias <= 10) {
+        return "Ferro";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        return "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        return "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        return "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        return "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        return "Lendário";
+    } else if (saldoVitorias >= 101) {
+        return "Imortal";
+    } 
+}
